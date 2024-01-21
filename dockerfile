2fa1c0c8ad8da24cf7319e35b7a964cfdbc5bcbc
tawat-ki <tawat.kiate@gmail.com>
2024-01-21 15:01:03 +0700

    fix: fix setup.sh

--- dockerfile
+++ dockerfile
@@ -1,59 +1,15 @@
 # FROM ubuntu:20.04
-FROM nvidia/cuda:11.6.1-devel-ubuntu20.04
+FROM nvidia/cuda:11.1.1-devel-ubuntu20.04
 
 
 # Set the working directory to /app
 WORKDIR /app
 RUN apt update 
 ENV DEBIAN_FRONTEND=noninteractive
-RUN apt install -y  python3-opencv libopencv-dev python3-pip git
-# Install required dependencies
-
-
-# Install PyTorch and torchvision
-RUN pip install torch torchvision torchaudio
-
-
-RUN pip install addict cython numpy albumentations==0.3.2 imagecorruptions matplotlib Pillow==6.2.2 six terminaltables pytest pytest-cov pytest-runner mmlvis scipy scikit-learn yapf
-RUN pip install Cython==0.29.33
-RUN pip install mmpycocotools
-RUN pip install nltk lmdb editdistance opencv-python requests onnx SharedArray tqdm pyclipper imgaug==0.3.0 Shapely Polygon3 scikit-image prettytable transformers seqeval Levenshtein networkx bs4 distance apted lxml jsonlines
-RUN pip install mmcv-full==1.3.4
-RUN pip install mmdet==2.11.0
+RUN apt install -y  python3-opencv libopencv-dev python3-pip git cmake
+RUN pip --default-timeout=1000 install torch==1.8.2 torchvision==0.9.2 torchaudio==0.8.2 --extra-index-url https://download.pytorch.org/whl/lts/1.8/cu111
 
 RUN git clone https://github.com/tawat-ki/DAVAR-Lab-OCR.git
 WORKDIR /app/DAVAR-Lab-OCR
 RUN git checkout docker
-
-RUN python3 setup.py develop
-RUN g++ -shared -o ./davarocr/davar_det/datasets/pipelines/lib/tp_data.so -fPIC ./davarocr/davar_det/datasets/pipelines/lib/tp_data.cpp -I/usr/include/opencv4
-RUN g++ -shared -o ./davarocr/davar_det/datasets/pipelines/lib/east_data.so -fPIC ./davarocr/davar_det/datasets/pipelines/lib/east_data.cpp -I/usr/include/opencv4
-RUN g++ -shared -o ./davarocr/davar_det/core/post_processing/lib/tp_points_generate.so -fPIC ./davarocr/davar_det/core/post_processing/lib/tp_points_generate.cpp -I/usr/include/opencv4
-RUN g++ -shared -o ./davarocr/davar_det/core/post_processing/lib/east_postprocess.so -fPIC ./davarocr/davar_det/core/post_processing/lib/east_postprocess.cpp -I/usr/include/opencv4
-RUN g++ -shared -o ./davarocr/davar_spotting/core/post_processing/lib/bfs_search.so -fPIC ./davarocr/davar_spotting/core/post_processing/lib/bfs_search.cpp -I/usr/include/opencv4
-RUN g++ -shared -o ./davarocr/davar_table/datasets/pipelines/lib/gpma_data.so -fPIC ./davarocr/davar_table/datasets/pipelines/lib/gpma_data.cpp -I/usr/include/opencv4
-RUN apt install -y cmake
-
-# if cuda version -ge 11
-WORKDIR /app/DAVAR-Lab-OCR/davarocr/davar_rcg/third_party/warp-ctc-pytorch_bindings/
-RUN sed -i 's|set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_30,code=sm_30 -O2")|# set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_30,code=sm_30 -O2")|' CMakeLists.txt
-RUN sed -i 's|set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_35,code=sm_35")|# set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_35,code=sm_35")|' CMakeLists.txt
-RUN sed -i 's|set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_50,code=sm_50")|# set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_50,code=sm_50")|' CMakeLists.txt
-WORKDIR /app/DAVAR-Lab-OCR/davarocr/davar_rcg/models/losses/
-RUN sed -i 's|        loss_warpctc = self\.loss_weight \* self\.criterion(log_probs,|        loss_warpctc = self\.loss_weight \* self\.criterion(log_probs\.cpu(),|' warpctc_loss.py
-#fi
-
-
-WORKDIR /app/DAVAR-Lab-OCR/davarocr/davar_rcg/third_party/warp-ctc-pytorch_bindings/src
-RUN rm ctc_entrypoint.cu
-RUN ln -s ctc_entrypoint.cpp ctc_entrypoint.cu
-WORKDIR /app/DAVAR-Lab-OCR/davarocr/davar_rcg/third_party/warp-ctc-pytorch_bindings/
-RUN mkdir build;
-WORKDIR /app/DAVAR-Lab-OCR/davarocr/davar_rcg/third_party/warp-ctc-pytorch_bindings/build
-RUN cmake ..
-RUN make
-
-WORKDIR /app/DAVAR-Lab-OCR/davarocr/davar_rcg/third_party/warp-ctc-pytorch_bindings/pytorch_binding
-RUN python3 setup.py install
-WORKDIR /app
-RUN apt install neovim -y
+RUN bash setup.sh
--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+set -e
 ROOT=$(cd $(dirname $0) && pwd )
 echo $ROOT
-PYTHON=${PYTHON:-"python"}
+PYTHON=${PYTHON:-"python3"}
 PIP=${PIP:-"pip"}
 
 # install the dependencies
 # Requires torch>= 1.3.0 torchvision >= 0.4.1
 
 # Dependencies from mmcv and mmdetection
-$PIP install addict cython numpy albumentations==0.3.2 imagecorruptions matplotlib Pillow==6.2.2 six terminaltables pytest pytest-cov pytest-runner mmlvis scipy sklearn mmpycocotools yapf
+$PIP install cython==0.29.33
+$PIP install addict cython numpy albumentations==0.3.2 imagecorruptions matplotlib Pillow==6.2.2 six terminaltables pytest pytest-cov pytest-runner mmlvis scipy scikit-learn mmpycocotools yapf
 
 # Dependencies of DavarOCR
 $PIP install nltk lmdb editdistance opencv-python requests onnx SharedArray tqdm pyclipper imgaug==0.3.0 Shapely Polygon3 scikit-image prettytable transformers seqeval Levenshtein networkx bs4 distance apted lxml jsonlines
-
 ###### install mmcv ######
 $PIP install mmcv-full==1.3.4
 
@@ -23,12 +24,12 @@ $PIP install mmdet==2.11.0
 $PYTHON setup.py develop
 
 ###### Compile dependencies ######
-g++ -shared -o ./davarocr/davar_det/datasets/pipelines/lib/tp_data.so -fPIC ./davarocr/davar_det/datasets/pipelines/lib/tp_data.cpp `pkg-config --cflags --libs opencv`
-g++ -shared -o ./davarocr/davar_det/datasets/pipelines/lib/east_data.so -fPIC ./davarocr/davar_det/datasets/pipelines/lib/east_data.cpp `pkg-config --cflags --libs opencv`
-g++ -shared -o ./davarocr/davar_det/core/post_processing/lib/tp_points_generate.so -fPIC ./davarocr/davar_det/core/post_processing/lib/tp_points_generate.cpp `pkg-config --cflags --libs opencv`
-g++ -shared -o ./davarocr/davar_det/core/post_processing/lib/east_postprocess.so -fPIC ./davarocr/davar_det/core/post_processing/lib/east_postprocess.cpp `pkg-config --cflags --libs opencv`
-g++ -shared -o ./davarocr/davar_spotting/core/post_processing/lib/bfs_search.so -fPIC ./davarocr/davar_spotting/core/post_processing/lib/bfs_search.cpp `pkg-config --cflags --libs opencv`
-g++ -shared -o ./davarocr/davar_table/datasets/pipelines/lib/gpma_data.so -fPIC ./davarocr/davar_table/datasets/pipelines/lib/gpma_data.cpp `pkg-config --cflags --libs opencv`
+g++ -shared -o ./davarocr/davar_det/datasets/pipelines/lib/tp_data.so -fPIC ./davarocr/davar_det/datasets/pipelines/lib/tp_data.cpp -I/usr/include/opencv4
+g++ -shared -o ./davarocr/davar_det/datasets/pipelines/lib/east_data.so -fPIC ./davarocr/davar_det/datasets/pipelines/lib/east_data.cpp -I/usr/include/opencv4
+g++ -shared -o ./davarocr/davar_det/core/post_processing/lib/tp_points_generate.so -fPIC ./davarocr/davar_det/core/post_processing/lib/tp_points_generate.cpp -I/usr/include/opencv4
+g++ -shared -o ./davarocr/davar_det/core/post_processing/lib/east_postprocess.so -fPIC ./davarocr/davar_det/core/post_processing/lib/east_postprocess.cpp -I/usr/include/opencv4
+g++ -shared -o ./davarocr/davar_spotting/core/post_processing/lib/bfs_search.so -fPIC ./davarocr/davar_spotting/core/post_processing/lib/bfs_search.cpp -I/usr/include/opencv4
+g++ -shared -o ./davarocr/davar_table/datasets/pipelines/lib/gpma_data.so -fPIC ./davarocr/davar_table/datasets/pipelines/lib/gpma_data.cpp -I/usr/include/opencv4
 
 
 
@@ -44,9 +45,26 @@ if [[ $cuda_version -ge ${11} ]];then
    sed -i 's|set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_35,code=sm_35")|# set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_35,code=sm_35")|' CMakeLists.txt
 
    sed -i 's|set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_50,code=sm_50")|# set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_50,code=sm_50")|' CMakeLists.txt
-   
-   cd $ROOT/davarocr/davar_rcg/models/losses/
-   sed -i 's|        loss_warpctc = self\.loss_weight \* self\.criterion(log_probs,|        loss_warpctc = self\.loss_weight \* self\.criterion(log_probs\.cpu(),|' warpctc_loss.py
+
+   FIND_FILE="./CMakeLists.txt"
+   FIND_STR="IF (CUDA_VERSION GREATER 9.9)"
+   if [ `grep -c "$FIND_STR" $FIND_FILE` -ne '0' ];then
+       echo "The File Has Ever Been Installed!"
+   else
+       sed -i '53s/$/\nIF (CUDA_VERSION GREATER 9.9)/g' CMakeLists.txt
+
+       sed -i '54s/$/\n    set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_75,code=sm_75")/g' CMakeLists.txt
+
+       sed -i '55s/$/\nENDIF()/g' CMakeLists.txt
+
+       sed -i '56s/$/\n/g' CMakeLists.txt
+
+       sed -i '57s/$/\nIF (CUDA_VERSION GREATER 10.9)/g' CMakeLists.txt
+
+       sed -i '58s/$/\n    set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_80,code=sm_80")/g' CMakeLists.txt
+
+       sed -i '59s/$/\nENDIF()/g' CMakeLists.txt
+   fi
 fi
 
 cd $ROOT/davarocr/davar_rcg/third_party/warp-ctc-pytorch_bindings/src
@@ -55,7 +73,7 @@ ln -s ctc_entrypoint.cpp ctc_entrypoint.cu
 
 
 cd $ROOT/davarocr/davar_rcg/third_party/warp-ctc-pytorch_bindings/
-mkdir build;
+mkdir -p build;
 cd build;
 cmake ..
 make
